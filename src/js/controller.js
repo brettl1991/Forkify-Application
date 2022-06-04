b5399743cbea3e7663d3ae38eebc9b4c46e2a8fc
brettl1991 <brettl1991@gmail.com>
2022-06-04 18:23:43 +0100

    Implementing pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,14 +9,14 @@ import { async } from 'regenerator-runtime/runtime';
 import searchView from './views/searchView.js';
 
 //coming from parcel
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
     //Getting dynamically id from the url (window.location is the entire url)
-    const id = window.location.hash.slice(1);
+    const id = window.location.hash.slice();
     // console.log('id:', id);
     //Guard clause
     if (!id) return;
@@ -44,7 +44,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
     //Render results
     // console.log(model.state.search.results);
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
   } catch (err) {
     console.log(err);
   }
