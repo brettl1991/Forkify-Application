bbc096e587a084a2f46c53e89f573e84868a7330
brettl1991 <brettl1991@gmail.com>
2022-06-07 14:50:16 +0100

    Implement a parent view to avoid same markup for results and bookmark view

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,12 +2,13 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
-  render(data) {
+  render(data, render = true) {
     //check if data exist
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
     this._data = data; //stores randering recipe's data
     const markup = this._generateMarkup();
+    if (!render) return markup;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
@@ -30,7 +31,7 @@ export default class View {
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
       //isEqualNode compare the 2 arrays
-      // console.log(curEl, newEl.isEqualNode(curEl));
+      // console.log(curEl, newEl.isEqualNode(cu  rEl));
 
       //Updates change TEXT
       if (
