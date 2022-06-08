ab52e84d517de4673b317cc34b78ae451a141c11
brettl1991 <brettl1991@gmail.com>
2022-06-08 14:26:23 +0100

    Sending recipe data to the fokify api from the module

--- src/js/controller.js
+++ src/js/controller.js
@@ -99,9 +99,15 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
-  //Upload new recipe data
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    console.log(newRecipe);
+    //Upload new recipe data
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error('!!!', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 //Implement publisher subscriber pattern
