4c1f6538e1d2d7a535d8ef9a4f2dc920d1cf45fc
brettl1991 <brettl1991@gmail.com>
2022-06-08 16:54:18 +0100

    Store bookmarks to locale storage, mark the recipe as bookmared, render recipe in the recipe view

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '73c0d6f2-0b6f-4c91-97d2-17a60b7f670c';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import SearchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -105,6 +106,14 @@ const controlAddRecipe = async function (newRecipe) {
     //Upload new recipe data
     await model.uploadRecipe(newRecipe);
     console.log(model.state.recipe);
+    //Render recipe
+    recipeView.render(model.state.recipe);
+    //Success message
+    addRecipeView.renderMessage();
+    //Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
   } catch (err) {
     console.error('!!!', err);
     addRecipeView.renderError(err.message);
