5780d8fadd685be51d65f520f349178f49220624
brettl1991 <brettl1991@gmail.com>
2022-06-08 12:55:55 +0100

    Remove hidden from the upload a new recipe form

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import SearchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import PaginationView from './views/resultsView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
