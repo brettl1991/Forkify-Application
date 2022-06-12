7ca212d55091b9df301fc56a037fb5f44f51cb89
brettl1991 <brettl1991@gmail.com>
2022-06-12 14:58:52 +0100

    Add new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,6 +129,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 //Implement publisher subscriber pattern
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -138,5 +142,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
